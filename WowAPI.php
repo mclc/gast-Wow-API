074eb62bf38b57943d829b513ecdfd5beb9ae1e1
Erwan Guillon <erwan@guillon.me>
2011-07-12 10:44:30 +0200

    first commit

---
+++ WowAPI.php
@@ -0,0 +1,29 @@
+<?php
+
+class WowAPI
+{
+    public static function character($character_name, $region = 'eu', $realm = 'Medivh', $options = null)
+    {
+        
+    }
+    
+    public static function guild($guild_name, $region = 'eu', $realm = 'Medivh', $options = null)
+    {
+        
+    }
+    
+    public static function realm($region = 'eu', $realm = 'Medivh', $options = null)
+    {
+        
+    }
+    
+    public static function data($data_type, $item_id)
+    {
+        
+    }
+    
+    private function _call($url)
+    {
+        return $call;
+    }
+}
